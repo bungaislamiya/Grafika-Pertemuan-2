f20a7cfd514113d544ed1adb6354037bfa74c219
Bunga Islamiya Putri <97377281+bungaislamiya@users.noreply.github.com>
2022-01-11 17:00:06 +0700

    Membuat Lingkaran

---
+++ MembuatLingkaran.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.applet.Applet;
+import java.awt.Graphics;
+/**
+ *
+ * @author lenovo
+ */
+public class MembuatLingkaran extends Applet {
+
+    /**
+     * Initialization method that will be called after the applet is loaded
+     * into the browser.
+     */
+    public void paint(Graphics g) {
+        
+        //g.drawOval(x1)
+        g.drawOval(100,20,150,150);
+    }
+    // TODO overwrite start(), stop() and destroy() methods
+}
